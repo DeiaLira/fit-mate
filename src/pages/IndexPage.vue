a3d9899cd30c69d858d8adabf27f645660a38234
Andreia Lira <andreia.s.lira08@gmail.com>
2024-06-19 17:45:52 -0300

    Iniciando fitmate com quasar framework

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="flex bg-brown content">
+    <section class="row">
+      <div class="">
+      teste
+    </div>
+    </section>
+  </q-page>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'IndexPage'
+});
+</script>
+
+<style lang="sass">
+
+
+</style>
