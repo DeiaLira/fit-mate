00b7302a60be541ed33bea94b74546668e7b2409
Andreia Lira <andreia.s.lira08@gmail.com>
2024-06-24 12:11:13 -0300

    Adicionando detalhes do exercício

--- src/pages/TodayWorkoutPage.vue
+++ src/pages/TodayWorkoutPage.vue
@@ -3,7 +3,31 @@
         <section class="mainContent">
             <p class="text-h5 text-weight-light text-center q-mt-xl">Treino A:</p>
             <section class="todayExercises q-gutter-y-sm q-mt-lg">
-                <q-btn class="options full-width q-pt-lg q-pb-lg text-h6 text-weight-light" label="Agachamento Livre" no-caps />
+                <q-btn class="options full-width q-pt-lg q-pb-lg text-h6 text-weight-light" label="Agachamento Livre" no-caps @click="counter.alert = !counter.alert"/>
+                    <q-dialog v-model="counter.alert">
+                        <q-card class="details">
+                            <q-card-section>
+                                <div class="text-h6 text-center">Detalhes do exercício:</div>
+                            </q-card-section>
+
+                            <q-card-section class="q-pt-none">
+                                <div class="q-pr-xl q-pl-xl">
+                                    <p class="text-h6 text-weight-light text-center">4 séries de 12 repetições</p>
+                                    <p class="text-h6 text-weight-light text-center">Carga atual: 50kg</p>
+                                </div>
+                                <div class="video q-mt-xl">
+                                    <span class="material-icons" style="">play_circle_filled</span>
+                                    <p class="text-h6 text-weight-light q-pl-md q-mt-md">Vídeo da execução</p>
+                                </div>
+
+                            </q-card-section>
+
+                            <q-card-actions class="actionAlert">
+                                <q-btn class="actionBtn full-width q-mt-lg" unelevated rounded label="Alterar Carga"/>
+                                <q-btn class="actionBtn full-width q-mt-lg q-mb-lg" unelevated rounded label="Concluir exercício" v-close-popup/>
+                            </q-card-actions>
+                        </q-card>
+                    </q-dialog>
                 <q-btn class="options full-width q-pt-lg q-pb-lg text-h6 text-weight-light" label="Cadeira extensora" no-caps />
                 <q-btn class="options full-width q-pt-lg q-pb-lg text-h6 text-weight-light" label="Leg Press 45°" no-caps />
                 <q-btn class="options full-width q-pt-lg q-pb-lg text-h6 text-weight-light" label="Panturrilha em pé" no-caps />
@@ -17,11 +41,17 @@
     </q-page>
   </template>
   
-  <script setup>
+<script setup>
+    import {useCounterStore} from '../stores/example-store.js'
+
   defineOptions({
     name: 'TodayWorkoutPage'
   });
-  </script>
+
+  const counter = useCounterStore();
+
+
+</script>
   
 <style lang="sass">
     
@@ -37,7 +67,26 @@
 .pushBtn
     background-color: #1B1A17
     color: #E6D5B8
+
+.details
+    background-color: #1B1A17
+    color: #E6D5B8
+    .actionAlert
+        max-width: 60%
+        margin: 0 auto
+
+.video
+    display: flex
+    justify-content: center
+
+.material-icons
+    font-size: 48px
+
+.actionBtn
+    background-color: #E6D5B8
+    color: #1B1A17
+    
  
-  
+    
 </style>
   
--- src/stores/example-store.js
+++ src/stores/example-store.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia';
 
 export const useCounterStore = defineStore('counter', {
   state: () => ({
-    counter: 0,
+    alert: false,
   }),
   getters: {
     doubleCount: (state) => state.counter * 2,
